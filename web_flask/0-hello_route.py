cd39a6e6482c0104e09f6b76c5c68e2f56d28a75
JNRBK <rafeekmorgan@yahoo.com>
2024-04-21 00:39:56 +0200

    constructed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Flask Module"""
-from flask import Flask
 
+
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_world():
     """function that returns a hello message"""
     return "<p>Hello HBNB!</p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Flask Module"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """function that returns a hello message"""
+    return "<p>Hello HBNB!</p>"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def do_hbnb():
+    """return a message belongs to this route"""
+    return "<p>HBNB</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
